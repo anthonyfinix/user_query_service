49fa271c3d96b2c8f218ad40e98366cd9d00ee66
anthonyfinix <anthonyfinix@gmail.com>
2021-06-30 21:11:35 +0530

    updateuser directly access contact_details update"

--- src/entities/user/service/updateUser.ts
+++ src/entities/user/service/updateUser.ts
@@ -1,6 +1,6 @@
 import { connection, FilterQuery, UpdateQuery, QueryOptions, ClientSession } from "mongoose";
 import { ServiceResponseInterface } from "../../../models/serviceResponse.interface";
-import { ContactDetailsInterface } from "../../contact_details/models/contact_details";
+import ContactDetails, { ContactDetailsInterface } from "../../contact_details/models/contact_details";
 import updateContactDetails from "../../contact_details/services/updateContactDetails";
 import User, { UserInterface } from "../models/user";
 
@@ -21,30 +21,30 @@ export default async (filter: UserInterface, userNewDetails: UserInterface, opti
         session.endSession()
         return { error: new Error('could not get the user details'), message: "unsuccessful" }
     }
-    let contactDetailsUpdateResponse = await updateContactDetails({ _id: currentUserDetails.contact_details }, userNewDetails.contact_details)
-    if (contactDetailsUpdateResponse.error) {
-        await session.abortTransaction()
-        session.endSession()
-        return { error: contactDetailsUpdateResponse.error, message: "unsuccessful" }
-    }
-    // handle user details
-    userNewDetails.contact_details = currentUserDetails.contact_details;
-    try {
-        let newUser = await User.findOneAndUpdate(<FilterQuery<ContactDetailsInterface>><unknown>filter, <UpdateQuery<ContactDetailsInterface>><unknown>userNewDetails, { session })
-        if (!newUser) {
+    try{
+        await ContactDetails.findOneAndUpdate({_id:currentUserDetails.contact_details},userNewDetails.contact_details)
+        userNewDetails.contact_details = currentUserDetails.contact_details;
+        try {
+            let newUser = await User.findOneAndUpdate(<FilterQuery<ContactDetailsInterface>><unknown>filter, <UpdateQuery<ContactDetailsInterface>><unknown>userNewDetails, { session })
+            if (!newUser) {
+                await session.abortTransaction()
+                session.endSession()
+                return { error: new Error("error while updating user details"), message: "unsuccessful" }
+            }
+            if (!options.session) {
+                await session.commitTransaction()
+                session.endSession()
+            }
+            return { result: newUser, message: "successful" }
+        } catch (e) {
             await session.abortTransaction()
             session.endSession()
-            return { error: new Error("error while updating user details"), message: "unsuccessful" }
+            return { error: new Error("error updating user details"), message: "unsuccessful" }
         }
-        if (!options.session) {
-            await session.commitTransaction()
-            session.endSession()
-        }
-        return { result: newUser, message: "successful" }
-    } catch (e) {
+    }catch(e){
         await session.abortTransaction()
         session.endSession()
-        return { error: new Error("error updating user details"), message: "unsuccessful" }
+        return { error: e, message: "unsuccessful" }
     }
 
 }
